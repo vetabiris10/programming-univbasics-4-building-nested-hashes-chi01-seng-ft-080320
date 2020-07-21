fc942f5c31a68317cec26ebad7d8cb79054f2065
MARIUS MOLDOVAN <biris_21@yahoo.com>
2020-07-21 19:40:30 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -64,7 +64,7 @@ monopoly =
         :shortline_railroad {:mortgage_value "800"}
       }
 }
-  
+
   # When you start writing the implementation for this method, copy the Hash
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more

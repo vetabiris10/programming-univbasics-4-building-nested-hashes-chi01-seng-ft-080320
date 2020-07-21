de571ebfc84c3d63709b0a1c0786f2874bfeaf3c
MARIUS MOLDOVAN <biris_21@yahoo.com>
2020-07-21 19:55:52 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,6 +1,6 @@
 def base_hash
 monopoly =
-{ :railroads {}
+{ :railroads =>{}
   }
   # Remember implicit return! We're returning this new Hash _without_ the
   # keyword "return." Nice and neat.
@@ -9,10 +9,10 @@ end
 
 def monopoly_with_second_tier
 monopoly =
-  { :railroads {
-      :pieces "4",
-      :rent_in_dollars {},
-      :names {}
+  { :railroads =>{
+      :pieces => 4,
+      :rent_in_dollars =>{},
+      :names =>{}
   }
 }
 
@@ -24,21 +24,23 @@ monopoly =
 end
 
 def monopoly_with_third_tier
-monopoly =
-  { :railroads {
-      :pieces "4",
-      :rent_in_dollars {
-        :one_piece_owned "25",
-        :two_pieces_owned "50",
-        :three_pieces_owned "100",
-        :four_pieces_owned "200"
-      }
-      :names {
-        :reading_railroad {},
-        :pennsylvania_railroad {},
-        :b_and_0_railroad {},
-        :shortline_railroad {}
-      }
+  monopoly =
+    { :railroads => {
+        :pieces => 4,
+        :rent_in_dollars => {
+          :one_piece_owned => 25,
+          :two_pieces_owned => 50,
+          :three_pieces_owned => 100,
+          :four_pieces_owned => 200
+        }
+          }
+          :names => {
+            :reading_railroad =>{},
+            :pennsylvania_railroad =>{},
+            :b_and_0_railroad =>{},
+            :shortline_railroad =>{}
+          }
+    }
   # When you start writing the implementation for this method, copy the Hash
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
@@ -48,20 +50,19 @@ end
 
 def monopoly_with_fourth_tier
 monopoly =
-  { :railroads {
-      :pieces "4",
-      :rent_in_dollars {
-        :one_piece_owned "25",
-        :two_pieces_owned "50",
-        :three_pieces_owned "100",
-        :four_pieces_owned "200"
-      }
+  { :railroads => {
+      :pieces => 4,
+      :rent_in_dollars => { 
+        :one_piece_owned => 25, 
+        :two_pieces_owned => 50,
+        :three_pieces_owned => 100,
+        :four_pieces_owned => 200}
         }
-      :names {
-        :reading_railroad {:mortgage_value "100"},
-        :pennsylvania_railroad {:mortgage_value "200"},
-        :b_and_0_railroad {:mortgage_value "400"},
-        :shortline_railroad {:mortgage_value "800"}
+      :names => {
+        :reading_railroad => {:mortgage_value => 100},
+        :pennsylvania_railroad => {:mortgage_value => 200},
+        :b_and_0_railroad => {:mortgage_value => 400},
+        :shortline_railroad => {:mortgage_value => 800}
       }
 }
 

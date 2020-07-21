e5d742782d42e16b0a2ff0c5c73322ad3a575bbe
MARIUS MOLDOVAN <biris_21@yahoo.com>
2020-07-21 19:55:55 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -52,8 +52,8 @@ def monopoly_with_fourth_tier
 monopoly =
   { :railroads => {
       :pieces => 4,
-      :rent_in_dollars => { 
-        :one_piece_owned => 25, 
+      :rent_in_dollars => {
+        :one_piece_owned => 25,
         :two_pieces_owned => 50,
         :three_pieces_owned => 100,
         :four_pieces_owned => 200}
